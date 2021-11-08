e65f785728b8f6e69b96b5cb8b371edf974b3ee7
Eridro1411 <eropionpixel@gmail.com>
2021-11-08 11:43:58 +0100

    add_film_plus_controller

---
+++ back-end/chc_cinema_studio/app/Http/Controllers/FilmController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Film;
+
+class FilmController extends Controller
+{
+    public function create_film(Request $request) {
+        $newFilmData = json_decode($request->getContent());
+
+        $newFilm = new Film();
+        $newFilm -> title = $newFilmData -> title;
+        $newFilm -> poster = $newFilmData -> poster;
+        $newFilm -> image = $newFilmData -> image;
+        $newFilm -> duration = $newFilmData -> duration;
+        $newFilm -> year = $newFilmData -> year;
+        $newFilm -> plot = $newFilmData -> plot;
+        $newFilm -> rating = $newFilmData -> rating;
+    }
+}
